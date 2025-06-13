c0a350bd7605520c8f7475878273cf89e70a6b26
CHIKHI Samy Rabah <csamyrabah@gmail.com>
2025-06-13 21:52:40 +0200

    Navbar underlined display when entering any page fixed

--- Frontend/src/Components/Navbar/ListItems.jsx
+++ Frontend/src/Components/Navbar/ListItems.jsx
@@ -6,8 +6,7 @@ function ListItems({ name = "" , isActive = false , setIsActive}) {
   hover:text-amber-400 transition duration-500 ${isActive ? "text-amber-400" : ""}`
   
   return (
-    <li className={style}
-    onClick={setIsActive}>
+    <li className={style}>
       {name=="HOME" ? <Link to="/">{name}</Link>: <Link to={`/${name.toLowerCase().replace(/\s+/g, "")}`}>{name}</Link>}
       {isActive && <hr className="border-none w-[80%] h-1 rounded-lg bg-orange-700"/>}
     </li>
--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -3,12 +3,18 @@ import ListItems from "./ListItems";
 import logo from "../Assets/pizzaLogo.png";
 import hero from "../Assets/hero-bg.jpg";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import OrderButton from "../OrderButton";
 
 function Navbar() {
   const [isActive, setIsActive] = useState(0);
-  const MenuList = ["HOME", "MENU", "ABOUT", "BOOK TABLE"];
+  const MenuList = [
+    { name: "HOME", path: "/" },
+    { name: "MENU", path: "/menu" },
+    { name: "ABOUT", path: "/about" },
+    { name: "BOOK TABLE", path: "/booktable" },
+  ];
+  const location = useLocation();
   return (
     <div
       className="flex justify-around shadow-sm text-gray-200 z-10"
@@ -21,16 +27,14 @@ function Navbar() {
         </Link>
       </div>
       <ul className="flex items-center lg:gap-12  text-xl font-medium mr-[5%]">
-        {MenuList.map((Item, index) => {
-          return (
-            <ListItems
-              name={Item}
-              key={index}
-              isActive={index == isActive}
-              setIsActive={() => setIsActive(index)}
-            />
-          );
-        })}
+        {MenuList.map((item, index) => (
+          <ListItems
+            key={index}
+            name={item.name}
+            to={item.path}
+            isActive={location.pathname === item.path}
+          />
+        ))}
       </ul>
       <div className="flex items-center mr-[5%] ">
         <OrderButton></OrderButton>

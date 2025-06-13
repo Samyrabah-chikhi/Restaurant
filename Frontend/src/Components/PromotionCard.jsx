48e53a0c3774f07a885f1f59fde19afbc70eb9ca
CHIKHI Samy Rabah <csamyrabah@gmail.com>
2025-06-13 22:40:25 +0200

    Image animation added

--- Frontend/src/Components/PromotionCard.jsx
+++ Frontend/src/Components/PromotionCard.jsx
@@ -4,11 +4,14 @@ import OrderButton from "./OrderButton";
 export default function PromotionCard({ title, percentage, image }) {
   return (
     <div className="bg-gray-800 flex gap-4 w-[500px] h-[200px] rounded-lg p-5 text-white">
-      <img
-        src={image}
-        alt={title}
-        className="w-40 h-40 rounded-full border-4 border-amber-300"
-      />
+      <div className="w-40 h-40 rounded-full border-4 border-amber-300 overflow-hidden">
+        <img
+          src={image}
+          alt={title}
+          className="w-full h-full object-cover transition-transform duration-200 hover:scale-125"
+        />
+      </div>
+
       <div className="flex flex-col justify-center gap-3 ">
         <h2 className="text-2xl">{title}</h2>
         <div className="flex items-end gap-1">
